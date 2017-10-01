32d669c7cba2b5bf9e4b54fc833e5dcc26f2d185
DroidFreak32 <rushabshah32@gmail.com>
2017-10-02 03:47:39 +0530

    Add media_profiles.xml

--- zanin/zanin-vendor-blobs.mk
+++ zanin/zanin-vendor-blobs.mk
@@ -137,6 +137,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/zanin/proprietary/etc/gps.conf:system/etc/gps.conf \
 	vendor/samsung/zanin/proprietary/etc/gps/glconfig.xml:system/etc/gps/glconfig.xml \
 	vendor/samsung/zanin/proprietary/etc/media_codecs.xml:system/etc/media_codecs.xml \
+	vendor/samsung/zanin/proprietary/etc/media_profiles.xml:system/etc/media_profiles.xml \
 	vendor/samsung/zanin/proprietary/etc/srm.bin:system/etc/srm.bin \
 	vendor/samsung/zanin/proprietary/etc/usb_portd.conf:system/etc/usb_portd.conf \
 	vendor/samsung/zanin/proprietary/etc/Volume.db:system/etc/Volume.db \
